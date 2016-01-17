a7106d28595507f696e71f3fc986356238542e1d
Nadeem Haidar <nadeem.haidar@mail.utoronto.ca>
2016-01-17 16:05:15 -0500

    Synchronized versions

---
+++ Syncify GUI/ConnectionMenu.cs
@@ -0,0 +1,304 @@
+﻿using SpotifyAPI.Local;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Syncify_GUI
+{
+    public partial class ConnectionMenu : Form
+    {
+        private const int PORT = 42069;
+
+        private MainMenu mainMenu;
+        private WebSocket client;
+        private WebSocketServer server;
+        private SpotifyLocalAPI spotify;
+
+        private int seconds = 0;
+
+        public ConnectionMenu(MainMenu mainMenu)
+        {
+            this.mainMenu = mainMenu;
+            spotify = mainMenu.spotify;
+            InitializeComponent();
+        }
+
+        private void ConnectionMenu_Load(object sender, EventArgs e)
+        {
+            // get IP's for status bar
+            var externalip = new System.Net.WebClient().DownloadString("http://bot.whatismyipaddress.com");
+            mainMenu.consoleWrite("Found WAN IP: " + externalip);
+            var localip = GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            if (localip == "")
+                localip = GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+            mainMenu.consoleWrite("Found LAN IP: " + localip);
+            status_ip.Text = "WAN: (" + externalip + ") LAN: (" + localip + ")";
+
+            //var process = Process.GetProcessesByName("Spotify");
+
+            mainMenu.consoleWrite("Currently playing: " + getSongName() + " by " + getArtistName());
+            pictureBox_album.Image = spotify.GetStatus().Track.GetAlbumArt(SpotifyAPI.Local.Enums.AlbumArtSize.Size640);
+            label_song_name.Text = getSongName();
+            label_artist_name.Text = getArtistName();
+            label_seek_time.Text = getPosition();
+
+
+            spotify.ListenForEvents = true;
+            spotify.OnTrackChange += Spotify_OnTrackChange;
+            spotify.OnTrackTimeChange += Spotify_OnTrackTimeChange;
+        }
+
+        private void button_start_server_Click(object sender, EventArgs e)
+        {
+            
+            button_clientmode.Enabled = !button_clientmode.Enabled;
+            if (button_clientmode.Enabled)
+            {
+                // Stop server
+                stop_host();
+                mainMenu.consoleWrite("Stopping Server..........OK");
+                ui_defaults();
+            }
+            else
+            {
+                // Start server
+                start_host(PORT);
+                mainMenu.consoleWrite("Starting server..........OK");
+                mainMenu.consoleWrite("Server listening on port " + PORT);
+                ui_hostmode();
+            }
+        }
+
+        private void button_clientmode_Click(object sender, EventArgs e)
+        {
+            // check if given ip has any blanks
+            if (ipAddressControl_client.AnyBlank)
+                {
+                mainMenu.consoleWrite("ERROR: Please enter a valid IP!");
+                SystemSounds.Hand.Play();
+                return;
+            }
+
+            button_hostmode.Enabled = !button_hostmode.Enabled;
+            if (button_hostmode.Enabled)
+            {
+                // Disconnect from server
+                mainMenu.consoleWrite("Disconnecting...");
+                stop_client();
+                //ui_defaults();
+            } 
+            else
+            {
+                // Connect to server
+                
+                var serverip = ipAddressControl_client.IPAddress;
+                mainMenu.consoleWrite("Connecting to " + serverip + "...");
+                start_client(serverip.ToString(), PORT);
+                //mainMenu.consoleWrite("Connected to " + serverip + ":" + PORT + "!");
+                //ui_clientmode();
+            }
+        }
+
+        // ui modification
+
+        private void ui_defaults()
+        {
+            button_hostmode.Enabled = true;
+            button_hostmode.Text = "Host";
+            button_hostmode.BackColor = Color.FromArgb(40, 40, 40);
+            button_hostmode.ForeColor = Color.FromArgb(232, 232, 232);
+
+            button_clientmode.Enabled = true;
+            button_clientmode.Text = "Connect";
+            button_clientmode.BackColor = Color.FromArgb(40, 40, 40);
+            button_clientmode.ForeColor = Color.FromArgb(232, 232, 232);
+            ipAddressControl_client.Enabled = true;
+        }
+
+        private void ui_hostmode()
+        {
+            button_hostmode.Text = "Stop Hosting";
+            button_hostmode.BackColor = Color.FromArgb(128, 173, 0);
+            button_hostmode.FlatAppearance.BorderColor = Color.FromArgb(232, 232, 232);
+            button_hostmode.ForeColor = Color.FromArgb(44, 57, 0);
+
+            button_clientmode.Text = "Connect disabled";
+            button_clientmode.BackColor = Color.FromArgb(50, 50, 50);
+
+            ipAddressControl_client.Enabled = false;
+        }
+
+        private void ui_clientmode()
+        {
+            button_clientmode.Text = "Disconnect";
+            button_clientmode.BackColor = Color.FromArgb(128, 173, 0);
+            button_clientmode.FlatAppearance.BorderColor = Color.FromArgb(232, 232, 232);
+            button_clientmode.ForeColor = Color.FromArgb(44, 57, 0);
+
+            ipAddressControl_client.Enabled = false;
+
+            button_hostmode.Text = "Hosting disabled";
+            button_hostmode.BackColor = Color.FromArgb(50, 50, 50);
+        }
+
+        // server-client code
+
+        private void start_host(int port)
+        {
+            server = new WebSocketServer(port);
+            server.AddWebSocketService<SyncifyWebSocket>("/syncify");
+            server.Start();
+        }
+
+        private void stop_host()
+        {
+            server.Stop();
+        }
+
+        private void start_client(string ip, int SERVER_PORT)
+        {
+            client = new WebSocket("ws://" + ip + ":" + SERVER_PORT + "/syncify");
+            client.OnOpen += (sender, e) => {
+                mainMenu.consoleWrite("NETWORK: Connection created");
+                ui_clientmode();
+            };
+            client.OnClose += (sender, e) => {
+                mainMenu.consoleWrite("NETWORK: Connection closed");
+                ui_defaults();
+            };
+            client.OnMessage += (sender, e) => {
+                mainMenu.consoleWrite("NETWORK: MSG FROM SVR: " + e.Data);
+                if (e.Data == "PAUSE")
+                {
+                    spotify.Pause();
+                }
+                else if (e.Data == "PLAY")
+                {
+                    spotify.Play();
+                }
+                else
+                {
+                    spotify.PlayURL(e.Data);
+                } 
+            };
+            client.OnError += (sender, e) => {
+                mainMenu.consoleWrite("NETWORK: ERROR");
+            };
+            client.Connect();
+        }
+
+        private void stop_client()
+        {
+            client.Close();
+        }
+
+        public string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
+        }
+
+        // spotify listeners
+
+        private void Spotify_OnTrackChange(TrackChangeEventArgs e)
+        {
+            //var new_image =  spotify.GetStatus().Track.GetAlbumArt(SpotifyAPI.Local.Enums.AlbumArtSize.Size640);
+            //string new_name = getSongName();
+            //string new_artist = getArtistName();
+            
+            seconds = 0;
+            this.BeginInvoke((Action)delegate ()
+            {
+                var new_image =  spotify.GetStatus().Track.GetAlbumArt(SpotifyAPI.Local.Enums.AlbumArtSize.Size640);
+                string new_name = getSongName();
+                string new_artist = getArtistName();
+                pictureBox_album.Image = new_image;
+                label_song_name.Text = new_name;
+                label_artist_name.Text = new_artist;
+                mainMenu.consoleWrite("Track changed: " + new_name + " by " + new_artist);
+                if (server != null && server.IsListening)
+                {
+                    server.WebSocketServices.Broadcast(e.NewTrack.TrackResource.Uri);
+                    mainMenu.consoleWrite("Broadcasting track change to clients");
+                }
+            });
+            /*
+            pictureBox_album.Invoke((MethodInvoker)(() => pictureBox_album.Image = new_image));
+            label_song_name.Invoke((MethodInvoker)(() => label_song_name.Text = new_name));
+            label_artist_name.Invoke((MethodInvoker)(() => label_artist_name.Text = new_artist));
+            this.Invoke((MethodInvoker)delegate
+            {
+                pictureBox_album.Image = new_image;
+                label_song_name.Text = new_name;
+                label_artist_name.Text = new_artist;
+                mainMenu.consoleWrite("Track changed: " + new_name + " by " + new_artist);
+            });  */
+        }
+
+        private void Spotify_OnTrackTimeChange(TrackTimeChangeEventArgs e)
+        {
+            //string new_time = getPosition();
+            if (!label_seek_time.IsDisposed && (int) e.TrackTime > seconds)
+            {
+                seconds = (int) e.TrackTime;
+                label_seek_time.Invoke((MethodInvoker)(() => label_seek_time.Text = TimeSpan.FromSeconds(seconds).ToString(@"mm\:ss")));
+            }
+                
+        }
+
+        // handy spotify functions
+
+        private String getSongName()
+        {
+            SpotifyAPI.Local.Models.Track track = spotify.GetStatus().Track;
+            return track.TrackResource.Name;
+        }
+
+        private string getArtistName()
+        {
+            SpotifyAPI.Local.Models.Track track = spotify.GetStatus().Track;
+            return track.ArtistResource.Name;
+        }
+
+        private String getTrackLength()
+        {
+            var track = spotify.GetStatus().Track;
+            double seconds = track.Length;
+            var time = TimeSpan.FromSeconds(seconds);
+            return time.ToString(@"mm\:ss");
+        }
+
+        private String getPosition()
+        {
+            double seconds = spotify.GetStatus().PlayingPosition;
+            var time = TimeSpan.FromSeconds(seconds);
+            return time.ToString(@"mm\:ss");
+        }
+
+    }
+}
---
+++ Syncify GUI/MainMenu.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Syncify_GUI
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.button_connect_spotify = new System.Windows.Forms.Button();
+            this.picture_title = new System.Windows.Forms.PictureBox();
+            this.label_version = new System.Windows.Forms.Label();
+            this.button_about = new System.Windows.Forms.Button();
+            this.status_console = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox_console = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_title)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_connect_spotify
+            // 
+            this.button_connect_spotify.FlatAppearance.BorderSize = 2;
+            this.button_connect_spotify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_connect_spotify.Font = new System.Drawing.Font("Gotham Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_connect_spotify.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.button_connect_spotify.Location = new System.Drawing.Point(12, 231);
+            this.button_connect_spotify.Name = "button_connect_spotify";
+            this.button_connect_spotify.Size = new System.Drawing.Size(295, 47);
+            this.button_connect_spotify.TabIndex = 1;
+            this.button_connect_spotify.Text = "Connect to Spotify";
+            this.button_connect_spotify.UseVisualStyleBackColor = true;
+            this.button_connect_spotify.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // picture_title
+            // 
+            this.picture_title.Image = ((System.Drawing.Image)(resources.GetObject("picture_title.Image")));
+            this.picture_title.Location = new System.Drawing.Point(0, 12);
+            this.picture_title.Name = "picture_title";
+            this.picture_title.Size = new System.Drawing.Size(295, 93);
+            this.picture_title.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_title.TabIndex = 2;
+            this.picture_title.TabStop = false;
+            // 
+            // label_version
+            // 
+            this.label_version.AutoSize = true;
+            this.label_version.Font = new System.Drawing.Font("Gotham Bold", 12F, System.Drawing.FontStyle.Bold);
+            this.label_version.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.label_version.Location = new System.Drawing.Point(256, 310);
+            this.label_version.Name = "label_version";
+            this.label_version.Size = new System.Drawing.Size(63, 18);
+            this.label_version.TabIndex = 3;
+            this.label_version.Text = "v0.01a";
+            this.label_version.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // button_about
+            // 
+            this.button_about.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_about.Font = new System.Drawing.Font("Gotham Bold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_about.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.button_about.Location = new System.Drawing.Point(12, 284);
+            this.button_about.Name = "button_about";
+            this.button_about.Size = new System.Drawing.Size(75, 23);
+            this.button_about.TabIndex = 4;
+            this.button_about.Text = "About";
+            this.button_about.UseVisualStyleBackColor = true;
+            this.button_about.Click += new System.EventHandler(this.button_about_Click);
+            // 
+            // status_console
+            // 
+            this.status_console.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.status_console.Name = "status_console";
+            this.status_console.Size = new System.Drawing.Size(115, 17);
+            this.status_console.Text = "Welcome to Syncify!";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status_console});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 328);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(319, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox_console
+            // 
+            this.textBox_console.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.textBox_console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_console.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_console.Location = new System.Drawing.Point(12, 116);
+            this.textBox_console.Multiline = true;
+            this.textBox_console.Name = "textBox_console";
+            this.textBox_console.ReadOnly = true;
+            this.textBox_console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_console.Size = new System.Drawing.Size(295, 102);
+            this.textBox_console.TabIndex = 6;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(319, 350);
+            this.Controls.Add(this.textBox_console);
+            this.Controls.Add(this.button_about);
+            this.Controls.Add(this.label_version);
+            this.Controls.Add(this.picture_title);
+            this.Controls.Add(this.button_connect_spotify);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainMenu";
+            this.Text = "Syncify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture_title)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status_console;
+        private System.Windows.Forms.Button button_about;
+        private System.Windows.Forms.Label label_version;
+        private System.Windows.Forms.PictureBox picture_title;
+        private System.Windows.Forms.Button button_connect_spotify;
+        private System.Windows.Forms.TextBox textBox_console;
+    }
+}
+
